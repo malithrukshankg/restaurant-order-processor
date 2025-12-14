85f96fa54cb5367242eed562edabf5e39e730dd4
malithrukshan <mcrukshan75@gmail.com>
2025-12-14 11:41:15 +1100

    bug: Fixed auhtetication issue in menu route

--- backend/src/routes/menuRoutes.ts
+++ backend/src/routes/menuRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { menuController } from "../controllers/menuController";
+import { validateBody } from "../middleware/validation";
+import { requireRole } from "../middleware/requireRole";
+import { authMiddleware } from "../middleware/auth";
+
 
 const router = Router();
 
-router.get("/", menuController.getAll);
+router.get("/", authMiddleware, requireRole("customer"), menuController.getAll);
 
 export default router;
